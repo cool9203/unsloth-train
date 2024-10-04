cf5152335cd9bfa1cd50dc967433df25b91781f6
ychsu <ychsu@iii.org.tw>
2024-10-04 16:30:04 +0800

    Update: temp

---
+++ src/train.py
@@ -0,0 +1,148 @@
+# coding: utf-8
+
+from pathlib import Path
+from typing import Union
+
+import torch
+from datasets import load_dataset
+from transformers import DataCollatorForSeq2Seq, TrainingArguments
+from trl import SFTTrainer
+from unsloth import FastLanguageModel, is_bfloat16_supported
+from unsloth.chat_templates import get_chat_template, standardize_sharegpt, train_on_responses_only
+
+max_seq_length = 2048  # Choose any! We auto support RoPE Scaling internally!
+dtype = None  # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
+load_in_4bit = True  # Use 4bit quantization to reduce memory usage. Can be False.
+seed = 3407
+
+# 4bit pre quantized models we support for 4x faster downloading + no OOMs.
+fourbit_models = [
+    "unsloth/Meta-Llama-3.1-8B-bnb-4bit",  # Llama-3.1 2x faster
+    "unsloth/Meta-Llama-3.1-8B-Instruct-bnb-4bit",
+    "unsloth/Meta-Llama-3.1-70B-bnb-4bit",
+    "unsloth/Meta-Llama-3.1-405B-bnb-4bit",  # 4bit for 405b!
+    "unsloth/Mistral-Small-Instruct-2409",  # Mistral 22b 2x faster!
+    "unsloth/mistral-7b-instruct-v0.3-bnb-4bit",
+    "unsloth/Phi-3.5-mini-instruct",  # Phi-3.5 2x faster!
+    "unsloth/Phi-3-medium-4k-instruct",
+    "unsloth/gemma-2-9b-bnb-4bit",
+    "unsloth/gemma-2-27b-bnb-4bit",  # Gemma 2x faster!
+    "unsloth/Llama-3.2-1B-bnb-4bit",  # NEW! Llama 3.2 models
+    "unsloth/Llama-3.2-1B-Instruct-bnb-4bit",
+    "unsloth/Llama-3.2-3B-bnb-4bit",
+    "unsloth/Llama-3.2-3B-Instruct-bnb-4bit",
+]  # More models at https://huggingface.co/unsloth
+
+
+def train_model(
+    save_path: Union[str, Path],
+    save_model_name: str,
+    model_format: str,
+    quantization_method: str,
+):
+    model, tokenizer = FastLanguageModel.from_pretrained(
+        # model_name="unsloth/Llama-3.2-1B-Instruct",  # or choose "unsloth/Llama-3.2-1B-Instruct"
+        model_name="unsloth/Llama-3.2-1B-bnb-4bit",
+        max_seq_length=max_seq_length,
+        dtype=dtype,
+        load_in_4bit=load_in_4bit,
+        # token = "hf_...", # use one if using gated models like meta-llama/Llama-2-7b-hf
+    )
+
+    model = FastLanguageModel.get_peft_model(
+        model,
+        r=16,  # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
+        target_modules=[
+            "q_proj",
+            "k_proj",
+            "v_proj",
+            "o_proj",
+            "gate_proj",
+            "up_proj",
+            "down_proj",
+        ],
+        lora_alpha=16,
+        lora_dropout=0,  # Supports any, but = 0 is optimized
+        bias="none",  # Supports any, but = "none" is optimized
+        # [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
+        use_gradient_checkpointing="unsloth",  # True or "unsloth" for very long context
+        random_state=seed,
+        use_rslora=False,  # We support rank stabilized LoRA
+        loftq_config=None,  # And LoftQ
+    )
+
+    tokenizer = get_chat_template(
+        tokenizer,
+        chat_template="llama-3.1",
+    )
+
+    def formatting_prompts_func(examples):
+        convos = examples["conversations"]
+        texts = [tokenizer.apply_chat_template(convo, tokenize=False, add_generation_prompt=False) for convo in convos]
+        return {
+            "text": texts,
+        }
+
+    dataset = load_dataset("mlabonne/FineTome-100k", split="train")
+    dataset = standardize_sharegpt(dataset)
+    dataset = dataset.map(
+        formatting_prompts_func,
+        batched=True,
+    )
+
+    trainer = SFTTrainer(
+        model=model,
+        tokenizer=tokenizer,
+        train_dataset=dataset,
+        dataset_text_field="text",
+        max_seq_length=max_seq_length,
+        data_collator=DataCollatorForSeq2Seq(tokenizer=tokenizer),
+        dataset_num_proc=2,
+        packing=False,  # Can make training 5x faster for short sequences.
+        args=TrainingArguments(
+            per_device_train_batch_size=2,
+            gradient_accumulation_steps=4,
+            warmup_steps=5,
+            # num_train_epochs = 1, # Set this for 1 full training run.
+            max_steps=60,
+            learning_rate=2e-4,
+            fp16=not is_bfloat16_supported(),
+            bf16=is_bfloat16_supported(),
+            logging_steps=1,
+            optim="adamw_8bit",
+            weight_decay=0.01,
+            lr_scheduler_type="linear",
+            seed=seed,
+            output_dir="outputs",
+        ),
+    )
+
+    trainer = train_on_responses_only(
+        trainer,
+        instruction_part="<|start_header_id|>user<|end_header_id|>\n\n",
+        response_part="<|start_header_id|>assistant<|end_header_id|>\n\n",
+    )
+
+    trainer_stats = trainer.train()
+
+    save_model_path = Path(save_path, save_model_name)
+    save_model_path.mkdir(parents=True, exist_ok=True)
+    if model_format.lower() == "gguf":
+        model.save_pretrained_gguf(
+            f"{str(save_model_path)}",
+            tokenizer,
+            quantization_method=quantization_method,
+        )
+    else:
+        # Save - Transformers
+        model.save_pretrained(f"{str(save_model_path)}/lora_model")  # Local saving
+        tokenizer.save_pretrained("lora_model")
+
+
+if __name__ == "__main__":
+    train_model(
+        save_path="/mnt/d/models",
+        save_model_name="test",
+        model_format="gguf",
+        quantization_method="f16",
+    )

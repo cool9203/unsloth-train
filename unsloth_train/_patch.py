b45ce544078f2bf51f932434b91f5c8e1d7322ff
ychsu <ychsu@iii.org.tw>
2024-12-03 10:30:26 +0800

    Update: remove unused `_patch.py` code

--- unsloth_train/_patch.py
+++ unsloth_train/_patch.py
@@ -2,71 +2,7 @@
 
 from __future__ import print_function
 
-import string
-from pathlib import Path
-
 from unsloth.tokenizer_utils import logger
-
-
-class SafeFormatter(string.Formatter):
-    """Reference: https://stackoverflow.com/a/34033230"""
-
-    def vformat(self, format_string, args, kwargs):
-        args_len = len(args)  # for checking IndexError
-        tokens = []
-        for lit, name, spec, conv in self.parse(format_string):
-            # re-escape braces that parse() unescaped
-            lit = lit.replace("{", "{{").replace("}", "}}")
-            # only lit is non-None at the end of the string
-            if name is None:
-                tokens.append(lit)
-            else:
-                # but conv and spec are None if unused
-                conv = "!" + conv if conv else ""
-                spec = ":" + spec if spec else ""
-                # name includes indexing ([blah]) and attributes (.blah)
-                # so get just the first part
-                fp = name.split("[")[0].split(".")[0]
-                # treat as normal if fp is empty (an implicit
-                # positional arg), a digit (an explicit positional
-                # arg) or if it is in kwargs
-                if not fp or fp.isdigit() or fp in kwargs:
-                    tokens.extend([lit, "{", name, conv, spec, "}"])
-                # otherwise escape the braces
-                else:
-                    tokens.extend([lit, "{{", name, conv, spec, "}}"])
-        format_string = "".join(tokens)  # put the string back together
-        # finally call the default formatter
-        return string.Formatter.vformat(self, format_string, args, kwargs)
-
-
-def create_ollama_modelfile(tokenizer, gguf_location):
-    """
-    Creates an Ollama Modelfile.
-    Use ollama.create(model = "new_ollama_model", modelfile = modelfile)
-    """
-    modelfile = getattr(tokenizer, "_ollama_modelfile", None)
-    if modelfile is None:
-        return None
-
-    modelfile = modelfile.replace("{{", "⚫@✅#🦥").replace("}}", "⚡@🦥#⛵")
-
-    model_name = Path(gguf_location).name
-    if "__EOS_TOKEN__" in modelfile:
-        modelfile = SafeFormatter().format(
-            modelfile,
-            __FILE_LOCATION__=f"./{model_name}",
-            __EOS_TOKEN__=tokenizer.eos_token,
-        )
-    else:
-        modelfile = SafeFormatter().format(
-            modelfile,
-            __FILE_LOCATION__=f"./{model_name}",
-        )
-
-    modelfile = modelfile.replace("⚫@✅#🦥", "{{").replace("⚡@🦥#⛵", "}}").rstrip()
-
-    return modelfile
 
 
 def _fix_chat_template(chat_template):
@@ -90,9 +26,6 @@ def _fix_chat_template(chat_template):
         chat_template = chat_template[: where + len(endfor)] + after_endfor
     pass
     return chat_template
-
-
-pass
 
 
 def fix_chat_template(tokenizer):
@@ -157,6 +90,3 @@ def fix_chat_template(tokenizer):
         pass
     pass
     return chat_template
-
-
-pass
